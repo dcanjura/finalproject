10572aeb4f545fa1cb3c9db2ea86f24b64774255
Diego <diegoac053@gmail.com>
2022-07-25 16:38:54 -0600

    Web Config

--- src/main/java/com/canjura/finalproject/configuration/WebConfig.java
+++ src/main/java/com/canjura/finalproject/configuration/WebConfig.java
@@ -7,7 +7,6 @@ import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
-
 import java.util.List;
 import java.util.Optional;
 
