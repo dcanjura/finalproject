f6643aff1050173bd625f6cd8145e6d3647e4fd6
Diego <diegoac053@gmail.com>
2022-07-30 01:29:36 -0600

    Checkout Entity Config

---
+++ src/main/java/com/canjura/finalproject/entity/checkout/Checkout.java
@@ -0,0 +1,29 @@
+package com.canjura.finalproject.entity.checkout;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Transactional
+@Table(name = "checkout")
+@Getter @Setter
+public class Checkout {
+
+    @Id
+    @Column(name = "checkout_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String userEmail;
+    private String addressInfo;
+    private String paymentInfo;
+
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_checkout_id", referencedColumnName = "checkout_id")
+    private List<CheckoutProduct> products = new ArrayList<>();
+}
