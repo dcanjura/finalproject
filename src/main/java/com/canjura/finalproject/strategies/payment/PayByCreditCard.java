730ad9822153136ce19e1460bdeb48dceae8cfc7
Diego <diegoac053@gmail.com>
2022-07-30 01:37:04 -0600

    PayByCreditCard Mapper Class

---
+++ src/main/java/com/canjura/finalproject/strategies/payment/PayByCreditCard.java
@@ -0,0 +1,20 @@
+package com.canjura.finalproject.strategies.payment;
+
+import com.canjura.finalproject.entity.user.User;
+import com.canjura.finalproject.entity.user.UserPayment;
+
+import java.util.List;
+
+public class PayByCreditCard implements PayStrategy{
+
+    @Override
+    public UserPayment getPayment(User user, String type) {
+        List<UserPayment> payments = user.getPayments();
+        for(UserPayment payment : payments){
+            if(payment.getType().equalsIgnoreCase(type)){
+                return payment;
+            }
+        }
+        return null;
+    }
+}
