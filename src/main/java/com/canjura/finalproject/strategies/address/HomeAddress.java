9566e5e2a724eea0a0dc2e0a49d7d9ff33caa429
Diego <diegoac053@gmail.com>
2022-07-30 01:34:00 -0600

    HomeAddress Mapper Class Config

---
+++ src/main/java/com/canjura/finalproject/strategies/address/HomeAddress.java
@@ -0,0 +1,19 @@
+package com.canjura.finalproject.strategies.address;
+
+import com.canjura.finalproject.entity.user.User;
+import com.canjura.finalproject.entity.user.UserAddress;
+import java.util.List;
+
+public class HomeAddress implements AddressStrategy{
+
+    @Override
+    public UserAddress getAddress(User user, String type) {
+        List<UserAddress> addresses = user.getAddress();
+        for(UserAddress address : addresses){
+            if(address.getType().equalsIgnoreCase(type)){
+                return address;
+            }
+        }
+        return null;
+    }
+}
