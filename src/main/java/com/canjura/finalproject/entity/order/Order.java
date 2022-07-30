f7dc6b775272138e05884e0bdc78c19fef8aebd6
Diego <diegoac053@gmail.com>
2022-07-30 01:29:18 -0600

    Order Entity Config

---
+++ src/main/java/com/canjura/finalproject/entity/order/Order.java
@@ -0,0 +1,30 @@
+package com.canjura.finalproject.entity.order;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import javax.persistence.*;
+import javax.transaction.Transactional;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@NoArgsConstructor
+@Transactional
+@Table(name = "orders")
+@Getter @Setter
+public class Order {
+
+    @Id
+    @Column(name = "order_id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    private String userInfo;
+    private String userEmail;
+    private String paymentInfo;
+    @OneToMany(cascade = CascadeType.ALL)
+    @JoinColumn(name = "fk_order_id", referencedColumnName = "order_id")
+    private List<OrderProduct> orderProducts = new ArrayList<>();
+    private Double total;
+    private String deliveryInfo;
+}
