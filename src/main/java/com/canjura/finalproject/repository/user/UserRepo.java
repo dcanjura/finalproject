756976e78eb21c955427d3f2735d447b3a3af435
Diego <diegoac053@gmail.com>
2022-08-02 11:23:42 -0600

    UserRepo Config

--- src/main/java/com/canjura/finalproject/repository/user/UserRepo.java
+++ src/main/java/com/canjura/finalproject/repository/user/UserRepo.java
@@ -1,12 +1,10 @@
 package com.canjura.finalproject.repository.user;
 
-import com.canjura.finalproject.entity.user.User;
+import com.canjura.finalproject.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserRepo extends JpaRepository<User, Integer> {
-
     User findUserByEmail(String email);
-
 }
