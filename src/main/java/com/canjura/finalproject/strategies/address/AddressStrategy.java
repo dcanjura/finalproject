c2a54f2430ab6d5d5316f7f2865d10cf52d18907
Diego <diegoac053@gmail.com>
2022-08-02 11:14:27 -0600

    AddressStrategy Config

--- src/main/java/com/canjura/finalproject/strategies/address/AddressStrategy.java
+++ src/main/java/com/canjura/finalproject/strategies/address/AddressStrategy.java
@@ -1,7 +1,7 @@
 package com.canjura.finalproject.strategies.address;
 
-import com.canjura.finalproject.entity.user.User;
-import com.canjura.finalproject.entity.user.UserAddress;
+import com.canjura.finalproject.entity.User;
+import com.canjura.finalproject.entity.UserAddress;
 
 public interface AddressStrategy {
     UserAddress getAddress(User user, String type);
